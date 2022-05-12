1518d7ef54ae3ffedc5fcf3cee5a65dfa37b2448
thintv <thintv@vccloud.vn>
2022-05-12 12:03:48 +0700

    change format response

--- src/schema/user_schema.py
+++ src/schema/user_schema.py
@@ -1,6 +1,6 @@
 from marshmallow import Schema, fields
 
-from .project_schema import ProjectResponseSchema
+from .project_schema import ProjectSchema
 from .utils import *
 
 class UserOfProject(Schema):
@@ -21,7 +21,7 @@ class ListUser(Schema):
 
 
 class ProjectUser(Schema):
-    project = fields.Nested(ProjectResponseSchema)
+    project = fields.Nested(ProjectSchema)
     users = fields.List(fields.Nested(UserOfProject))
 
 
