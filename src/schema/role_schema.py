127f5cda2ffacbe98eea564b4af6cb98b22a9495
thintv <thintv@vccloud.vn>
2022-05-19 09:01:08 +0700

    add schema role

---
+++ src/schema/role_schema.py
@@ -0,0 +1,30 @@
+from marshmallow import Schema, fields
+
+from .resource_schema import *
+from .utils import *
+
+
+class RoleSchema(Schema):
+    uuid = fields.Str(metadata={"format": "uuid"})
+    name = fields.Str(metadata={"format": "string"})
+    description = fields.Str()
+    is_custom = fields.Boolean()
+    is_active = fields.Boolean()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    deleted_at = fields.DateTime()
+
+class ListRoleSchemaResponse(Schema):
+    success = fields.Boolean()
+    message = fields.Str()
+    error_code = fields.Int()
+    data = fields.List(fields.Nested(RoleSchema))
+    metadata = fields.Nested(Metadata)
+
+
+class GetRoleSchemaResponse(Schema):
+    success = fields.Boolean()
+    message = fields.Str()
+    error_code = fields.Int()
+    data = fields.List(fields.Nested(RoleSchema))
+    metadata = fields.Nested(Metadata)
